93cdea4805d7f1a64cfa643636a0ec749324101d
Shawn Long <slong024@ucr.edu>
2022-01-01 11:15:50 -0800

    Wrote dummy hello_http and created configured cloud build

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-def main():
-    return "OK"
+from flask import escape
+import functions_framework
+
+def hello_http(request):
+    """HTTP Cloud Function.he
+    Args:
+        request (flask.Request): The request object.
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using 'make_response'
+    """
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    elif request_args and 'name' in request_args:
+        name = request_args['name']
+    else:
+        name = 'World'
+    return f'Hello there {escape(name)}!'
