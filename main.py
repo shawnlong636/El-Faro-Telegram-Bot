0fd6233a7f76fb692cd69f059b55ad28f960bdcd
Shawn Long <slong024@ucr.edu>
2022-01-02 15:46:53 -0800

    Moved secret access to dedicated function and switched to secretmanager lib instead of using cloudbuild

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 from flask import escape
 import functions_framework
 from os import environ
+from google.cloud import secretmanager
+from sys import platform
+
+def get_secrets() -> {str: str}:
+    secrets = {"Telegram_API_Key": None,
+            "Telegram_API_Hash": None,
+            "Telegram_API_ID": None
+            }
+
+    project_id = environ["GCP_PROJECT"]
+    client = secretmanager.SecretManagerServiceClient()
+    
+    key_name = f"projects/{project_id}/secrets/Telegram_API_Key/versions/latest"
+    hash_name = f"projects/{project_id}/secrets/Telegram_API_Hash/versions/latest"
+    id_name = key = f"projects/{project_id}/secrets/Telegram_API_ID/versions/latest"
+    
+    response = client.access_secret_version(name=key_name)
+    secrets["Telegram_API_Key"] = response.payload.data.decode("UTF-8")
+
+    response = client.access_secret_version(name=hash_name)
+    secrets["Telegram_API_Hash"] = response.payload.data.decode("UTF-8")
+
+    response = client.access_secret_version(name=id_name)
+    secrets["Telegram_API_ID"] = response.payload.data.decode("UTF-8")
+
+    return secrets
 
 def hello_http(request):
     """HTTP Cloud Function.he
@@ -10,10 +36,11 @@ def hello_http(request):
         The response text, or any set of values that can be turned into a
         Response object using 'make_response'
     """
+
+    secrets = get_secrets()
+
     request_json = request.get_json(silent=True)
     request_args = request.args
-
-    Telegram_API_Key = environ['Telegram_API_Key']
 
     if request_json and 'name' in request_json:
         name = request_json['name']
