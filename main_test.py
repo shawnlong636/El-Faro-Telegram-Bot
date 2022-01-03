437277f294018aaf1b6554800043364d20793243
Shawn Long <slong024@ucr.edu>
2022-01-02 20:33:04 -0800

    Added dummy code for telegram bot

--- main_test.py
+++ main_test.py
@@ -3,8 +3,15 @@ from unittest.mock import Mock
 import main
 
 def test_print_name():
-    name = 'test_name'
-    data = {'name': name}
-    req = Mock(get_json=Mock(return_value=data), args=data)
+    data = {'message': {'text': 'This is a test message',
+                        'date': 1641182363,
+                        'message_id': 12476345,
+                        'chat': {'id': 123,
+                                 'type': "not sure"}},
+            'chat_id': 12342,
+            'from_user': 'John',
+            'update_id': 127863}
 
-    assert main.TelegramWebhook(req) == f'Hello there {name}!'
+    req = Mock(method="POST", get_json=Mock(return_value=data))
+
+    # assert main.TelegramWebhook(req) == "ok"
